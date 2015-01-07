2f686009ac4f163d2903bc0c9101227c2e395183
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-01-07 14:26:00 +0100

    Issue #1: Skip integration tests if memcached is not available

--- tests/Integration/Suites/MemcachedKeyValueStoreIntegrationTest.php
+++ tests/Integration/Suites/MemcachedKeyValueStoreIntegrationTest.php
@@ -4,6 +4,10 @@ namespace Brera\KeyValue\Memcached;
 
 class MemcachedKeyValueStoreIntegrationTest extends \PHPUnit_Framework_TestCase
 {
+	const MEMCACHED_HOST = 'localhost';
+
+	const MEMCACHED_PORT = 11211;
+
 	/**
 	 * @var MemcachedKeyValueStore
 	 */
@@ -11,8 +15,14 @@ class MemcachedKeyValueStoreIntegrationTest extends \PHPUnit_Framework_TestCase
 
 	protected function setUp()
 	{
+		if (!class_exists(\Memcached::class)) {
+			$this->markTestSkipped(
+				sprintf('Memcached is not available on %s:%s', self::MEMCACHED_HOST, self::MEMCACHED_PORT)
+			);
+		}
+
 		$client = new \Memcached();
-		$client->addServer('localhost', 11211);
+		$client->addServer(self::MEMCACHED_HOST, self::MEMCACHED_PORT);
 		$client->deleteMulti(['foo', 'key1', 'key2']);
 
 		$this->keyValueStore = new MemcachedKeyValueStore($client);
