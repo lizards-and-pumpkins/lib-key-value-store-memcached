72126cbdcc68851a2cdebd01a0813f3091df9801
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2016-02-27 21:36:29 +0100

    Issue #4: Replace tabs with spaces

--- src/MemcachedKeyValueStore.php
+++ src/MemcachedKeyValueStore.php
@@ -7,65 +7,65 @@ use LizardsAndPumpkins\DataPool\KeyValue\KeyValueStore;
 
 class MemcachedKeyValueStore implements KeyValueStore
 {
-	const MEMCACHED_RES_NOTFOUND = 16;
+    const MEMCACHED_RES_NOTFOUND = 16;
 
-	/**
-	 * @var \Memcached
-	 */
-	private $client;
+    /**
+     * @var \Memcached
+     */
+    private $client;
 
-	public function __construct(\Memcached $client)
-	{
-		$this->client = $client;
-	}
+    public function __construct(\Memcached $client)
+    {
+        $this->client = $client;
+    }
 
-	/**
-	 * @param string $key
-	 * @param mixed $value
-	 */
-	public function set($key, $value)
-	{
-		$this->client->set($key, $value);
-	}
+    /**
+     * @param string $key
+     * @param mixed $value
+     */
+    public function set($key, $value)
+    {
+        $this->client->set($key, $value);
+    }
 
-	/**
-	 * @param string $key
-	 * @return mixed
-	 */
-	public function get($key)
-	{
-		if (!$value = $this->client->get($key)) {
-			throw new KeyNotFoundException(sprintf('Key not found "%s"', $key));
-		}
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    public function get($key)
+    {
+        if (!$value = $this->client->get($key)) {
+            throw new KeyNotFoundException(sprintf('Key not found "%s"', $key));
+        }
 
-		return $value;
-	}
+        return $value;
+    }
 
-	/**
-	 * @param string $key
-	 * @return bool
-	 */
-	public function has($key)
-	{
-		$this->client->get($key);
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function has($key)
+    {
+        $this->client->get($key);
 
-		return self::MEMCACHED_RES_NOTFOUND !== $this->client->getResultCode();
-	}
+        return self::MEMCACHED_RES_NOTFOUND !== $this->client->getResultCode();
+    }
 
-	/**
-	 * @param string[] $keys
-	 * @return mixed[]
-	 */
-	public function multiGet(array $keys)
-	{
-		return $this->client->getMulti($keys);
-	}
+    /**
+     * @param string[] $keys
+     * @return mixed[]
+     */
+    public function multiGet(array $keys)
+    {
+        return $this->client->getMulti($keys);
+    }
 
-	/**
-	 * @param mixed[] $items
-	 */
-	public function multiSet(array $items)
-	{
-		$this->client->setMulti($items);
-	}
+    /**
+     * @param mixed[] $items
+     */
+    public function multiSet(array $items)
+    {
+        $this->client->setMulti($items);
+    }
 } 
--- tests/Integration/Suites/MemcachedKeyValueStoreIntegrationTest.php
+++ tests/Integration/Suites/MemcachedKeyValueStoreIntegrationTest.php
@@ -4,52 +4,52 @@ namespace LizardsAndPumpkins\DataPool\KeyValue\Memcached;
 
 class MemcachedKeyValueStoreIntegrationTest extends \PHPUnit_Framework_TestCase
 {
-	const MEMCACHED_HOST = 'localhost';
+    const MEMCACHED_HOST = 'localhost';
 
-	const MEMCACHED_PORT = 11211;
+    const MEMCACHED_PORT = 11211;
 
-	/**
-	 * @var MemcachedKeyValueStore
-	 */
-	private $keyValueStore;
+    /**
+     * @var MemcachedKeyValueStore
+     */
+    private $keyValueStore;
 
-	protected function setUp()
-	{
-		if (!class_exists(\Memcached::class)) {
-			$this->markTestSkipped(
-				sprintf('Memcached is not available on %s:%s', self::MEMCACHED_HOST, self::MEMCACHED_PORT)
-			);
-		}
+    protected function setUp()
+    {
+        if (!class_exists(\Memcached::class)) {
+            $this->markTestSkipped(
+                sprintf('Memcached is not available on %s:%s', self::MEMCACHED_HOST, self::MEMCACHED_PORT)
+            );
+        }
 
-		$client = new \Memcached();
-		$client->addServer(self::MEMCACHED_HOST, self::MEMCACHED_PORT);
-		$client->deleteMulti(['foo', 'key1', 'key2']);
+        $client = new \Memcached();
+        $client->addServer(self::MEMCACHED_HOST, self::MEMCACHED_PORT);
+        $client->deleteMulti(['foo', 'key1', 'key2']);
 
-		$this->keyValueStore = new MemcachedKeyValueStore($client);
-	}
+        $this->keyValueStore = new MemcachedKeyValueStore($client);
+    }
 
-	public function testValueIsSetAndRetrieved()
-	{
-		$this->keyValueStore->set('foo', 'bar');
-		$result = $this->keyValueStore->get('foo');
+    public function testValueIsSetAndRetrieved()
+    {
+        $this->keyValueStore->set('foo', 'bar');
+        $result = $this->keyValueStore->get('foo');
 
-		$this->assertEquals('bar', $result);
-	}
+        $this->assertEquals('bar', $result);
+    }
 
-	public function testMultipleValuesAreSetAndRetrieved()
-	{
-		$keys = ['key1', 'key2'];
-		$values = ['foo', 'bar'];
-		$items = array_combine($keys, $values);
+    public function testMultipleValuesAreSetAndRetrieved()
+    {
+        $keys = ['key1', 'key2'];
+        $values = ['foo', 'bar'];
+        $items = array_combine($keys, $values);
 
-		$this->keyValueStore->multiSet($items);
-		$result = $this->keyValueStore->multiGet($keys);
+        $this->keyValueStore->multiSet($items);
+        $result = $this->keyValueStore->multiGet($keys);
 
-		$this->assertSame($items, $result);
-	}
+        $this->assertSame($items, $result);
+    }
 
-	public function testFalseIsReturnedIfKeyDoesNotExist()
-	{
-		$this->assertFalse($this->keyValueStore->has('foo'));
-	}
+    public function testFalseIsReturnedIfKeyDoesNotExist()
+    {
+        $this->assertFalse($this->keyValueStore->has('foo'));
+    }
 }
